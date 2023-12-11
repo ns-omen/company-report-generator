6d440b100879bdccb02247b427b36b0ffe3ede5a
ns-omen <58156889+ns-omen@users.noreply.github.com>
2023-12-11 10:26:51 +0700

    Update ENV, API_BASE_URL Constants Retrieval Method (#2)
    change retrieval method for env and api base url

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-ENV = 'prd'
-API_BASE_URL = 'https://pmqutigs5m.execute-api.us-west-2.amazonaws.com'
+ENV = st.secrets['env']
+API_BASE_URL = st.secrets['api_base_url']
+API_KEY = st.secrets['api_key']
 GENERATE_COMPANY_REPORT_ENDPOINT = f'{API_BASE_URL}/{ENV}/generate-company-report'
 GET_PROGRESS_ENDPOINT = f'{API_BASE_URL}/{ENV}/get-progress?transactionID='
-API_KEY = st.secrets['api_key']
